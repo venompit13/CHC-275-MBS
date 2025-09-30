f84912fc26cd490386f6fc910b7c5a48cb899e9e
venompit13 <mitch@venompit.com>
2025-09-30 12:03:15 -0400

    Create asssignment1.py

---
+++ Assignments/asssignment1.py
@@ -0,0 +1,16 @@
+""" 
+
+Assignment 1
+
+Problem 1: hailstone.py
+
+The hailstone problem (collatz conjecture) is an open problem in the field of number theory in math
+
+Problem 2: fish.py
+
+if-elif-else: problem
+
+yo=u have one type of fish (cornivorous, salt water, community).
+
+Problem 3: powerlevels.py.
+"""
