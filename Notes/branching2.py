d8374f9af2eeb8b1a3e13d7eb23da635cff48ed0
venompit13 <mitch@venompit.com>
2025-09-30 12:07:08 -0400

    Create branching2.py

---
+++ Notes/branching2.py
@@ -0,0 +1,42 @@
+"branching2.py"
+
+"""
+let's discuss forom a software development standpoint how if-elifs can be used in a software design question
+
+when you launch a game you open to a main menu in our command liune programs
+
+whats an easy way to make a main menu?
+
+1. PRINT OUT ALL OF THE OPTIONS
+2. TAKE IN KEYBOARD INPUT
+3. USE IFs and ELIFs in order to select the option the user typed in
+
+freshman $5
+sophmores $7
+juniors $9
+seniors $11
+
+"""
+print("Welcome to the lunch loan program")
+print("Enter your grade level")
+print("1. Freshman")
+print("2. Sophmore")
+print("3. Junior")
+print("4. Senior")
+option = input("Enter your grade level: ")
+
+if (option == "freshman") or (option == "Freshman"):
+    print("You get $5")
+if (option == "sophmore") or (option == "Sophmore"):
+    print("You get $7")
+if (option == "junior") or (option == "Junior"):
+    print("You get $9")
+if (option == "senior") or (option == "Senior"):
+    print("You get $11")
+if (option == "Super Senior") or (option == "super senior"):
+    print("Go to college")
+
+""" 
+your goal is to match the output to the option using ifs and elifs
+"""
+
