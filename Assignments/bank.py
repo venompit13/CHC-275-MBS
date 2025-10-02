8b03b84daadc420be0a664ffebc5b752b5e5bffb
venompit13 <mitch@venompit.com>
2025-10-02 14:21:24 -0400

    Update bank.py

--- Assignments/bank.py
+++ Assignments/bank.py
@@ -1,5 +1,5 @@
 print("Welcome to FFCU Bank!")
-names = ["Mitch Scahuer", "Gabby Crespo", "Colin Lawton", "Paul Gage", "Asher Collins"]
+names = ["Mitch Schauer", "Gabby Crespo", "Colin Lawton", "Paul Gage", "Asher Collins"]
 balances = [10000, 15000, 20000, 30000, 1000]
 
 check = False
@@ -11,6 +11,7 @@ while check == False:
     print("3. Deposit?")
     print("4. Withdrawl?")
     print("5. Transfer Funds?")
+    print("6. Coming Soon...")
     option = input("Select option or quit to exit: ")
     if option == "1":
         name = input("Enter account name you want to add to the list: ")
@@ -26,8 +27,23 @@ while check == False:
     elif option == "3":
         name = input("Which account is depositing: ")
         index = names.index(name)
-        money = input("How much: ")
+        money = input("How much is being deposited: ")
         money = int(money)
         balances[index] = balances[index] + money
+    elif option == "4":
+        name = input("Which account is withdrawling: ")
+        index = names.index(name)
+        money = input("How much is being withdrawn: ")
+        money = int(money)
+        balances[index] = balances[index] - money
+    elif option == "5":
+        name = input("Which account is transferring: ")
+        index = names.index(name)
+        name2 = input("To which account: ")
+        index2 = names.index(name2)
+        transfer = input("How much is being transfered: ")
+        transfer = int(transfer)
+        balances[index] = balances[index] - transfer
+        balances[index2] = balances[index2] + transfer
     if option == "quit":
-        check = True
+        check = True 
