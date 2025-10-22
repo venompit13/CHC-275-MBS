02ec9902b01cfbdcb7525b9c789b8d2a8821f2a4
venompit13 <mitch@venompit.com>
2025-10-22 15:28:02 -0400

    Create palindrome.py

---
+++ Assignments/palindrome.py
@@ -0,0 +1,5 @@
+#strings behave like lists can use for-loops (pulls out individual character one by one)
+#when adding 2 strings if str1 is hello and str2 world add them str1 + str2 = hello world
+palindrome = input("Type in your palindrome: ")
+for char in palindrome:
+    print(palindrome)
