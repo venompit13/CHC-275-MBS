8ffcc7263b469ea1531cfb1f188ca0757a54666d
venompit13 <mitch@venompit.com>
2025-11-03 13:49:19 -0500

    Update fileio.py

--- Notes/fileio.py
+++ Notes/fileio.py
@@ -15,10 +15,20 @@ escape sequences decided when one line starts and another ends
 fileitself may not exist in system
 """
 
-file = open("names.txt", "r")
+file = open("Notes/names.txt", "r")
 names = file.readlines()
 print(names)
 for i in range(len(names)):
     names[i] = names[i].strip()
 print(names)
 file.close()
+
+""" 
+opening the file
+reading into list called buffer
+preproccessiong data such thatt we can use it in our porgram
+
+writting into a file?
+
+third mode opening file called append mode
+"""
