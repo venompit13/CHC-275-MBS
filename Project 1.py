dc332a4117f185bf4e78a05924e235ad69061664
venompit13 <mitch@venompit.com>
2025-12-01 11:26:37 -0500

    Update Project 1.py

--- Project 1.py
+++ Project 1.py
@@ -3,7 +3,6 @@ Docstring for Roulette
 My project will be a roulette like game using what we have learned throughout the year
 """
 import pygame
-
-
-pygame.Color(55, 110, 165, 255)
-pygame.display.init()
+import random
+pygame.init()
+pygame.Color()
