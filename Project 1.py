599b0e36ae06517473878624ad5c7ebec3436d9e
venompit13 <mitch@venompit.com>
2025-12-02 14:26:54 -0500

    Delete Project 1.py

--- Project 1.py
+++
@@ -1,9 +0,0 @@
-"""
-Docstring for Roulette
-My project will be a roulette like game using what we have learned throughout the year
-"""
-import pygame
-import random
-pygame.init()
-pygame.Color(55, 110, 165, a = 255)
-
