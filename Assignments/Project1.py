64d268dcbbd948920ac54dbe67720bf1a6edce19
MBS <mitch@venompit.com>
2025-12-18 16:45:40 -0500

    Update Project1.py

--- Assignments/Project1.py
+++ Assignments/Project1.py
@@ -13,24 +13,30 @@ height = 960
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Project Pong")
 
-pong = pygame.Rect(width/2 - 15,height/2-15,30,30)
+pong = pygame.Rect(width/2 - 15,height/2 - 15,30,30)
 player = pygame.Rect(width - 20,height/2 -70,10,140)
 opponent = pygame.Rect(10, height/2 - 70,10, 140)
  
 bg_color = pygame.Color('gray12')
 light_gray = (200,200,200)
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
 screen.fill(bg_color)
 pygame.draw.rect(screen,light_gray, player)
 pygame.draw.rect(screen,light_gray, opponent)
 pygame.draw.ellipse(screen, light_gray, pong)
 pygame.draw.aaline(screen, light_gray, (width/2,0), (width/2,height))
 
+pong_speed_x = 7
+pong_speed_y = 7
+
+pong.x += pong_speed_y
+pong.y += pong_speed_x
+
+
+
 pygame.display.flip()
 clock.tick(60)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
