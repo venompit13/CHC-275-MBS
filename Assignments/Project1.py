82b25c15c5a4b4df2b379f35b130dc773646a199
MBS <mitch@venompit.com>
2025-12-18 16:53:35 -0500

    Update Project1.py

--- Assignments/Project1.py
+++ Assignments/Project1.py
@@ -25,11 +25,15 @@ pygame.draw.rect(screen,light_gray, opponent)
 pygame.draw.ellipse(screen, light_gray, pong)
 pygame.draw.aaline(screen, light_gray, (width/2,0), (width/2,height))
 
-pong_speed_x = 70
-pong_speed_y = 70
+pong_speed_x = 7
+pong_speed_y = 7
 pong.x += pong_speed_y
 pong.y += pong_speed_x
 
+if pong.top <= 0 or pong.bottom >= height:
+    pong_speed_y *= -1
+if pong.left <= 0 or pong.right >= width:
+    pong_speed_x *= -1
 
 pygame.display.flip()
 clock.tick(60)
