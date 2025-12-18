657251a202c0f4a8071f9e8d8c1983b0fb7c659f
MBS <mitch@venompit.com>
2025-12-18 16:39:21 -0500

    Update Project1.py

--- Assignments/Project1.py
+++ Assignments/Project1.py
@@ -13,9 +13,18 @@ height = 960
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Project Pong")
 
-ball = pygame.Rect(30,30)
+pong = pygame.Rect(width/2 - 15,height/2-15,30,30)
+player = pygame.Rect(width - 20,height/2 -70,10,140)
+opponent = pygame.Rect(10, height/2 - 70,10, 140)
+ 
+color = pygame.Color('gray12')
+light_gray = (200,200,200)
 
-
+screen.fill(color)
+pygame.draw.rect(screen,light_gray, player)
+pygame.draw.rect(screen,light_gray, opponent)
+pygame.draw.ellipse(screen, light_gray, pong)
+pygame.draw.aaline(screen, light_gray, (width/2,0), (width/2,height))
 
 
 while True:
