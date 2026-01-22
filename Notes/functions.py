0a230bc9d19274f47e3b6f05b5cac40abb73bd0a
venompit13 <mitch@venompit.com>
2026-01-22 12:02:27 -0500

    Create functions.py

---
+++ Notes/functions.py
@@ -0,0 +1,21 @@
+#functions.py
+"""
+Drills starting this semester
+    - small program
+    - reading code and evaluating what the output is or whether there is an error
+
+be able to  write code once and use it twice or more
+f(x) = x^2
+^takes x as input and outputs x squared
+
+routines u can repeat that outputs something
+
+pros:
+    - we can reuse code rather than having to rewrite it all the time
+    - much more readable
+    
+cons:
+    - kinda difficult to undewrstand what is happening in the program
+    - we introduce a large level of abstraction that we ar enot used to so far
+    
+"""
