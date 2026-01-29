fd6828babdc53c66256da132e967e478cec5427d
MBS <mitch@venompit.com>
2026-01-29 10:42:29 -0500

    Create getMin.py

---
+++ Notes/getMin.py
@@ -0,0 +1,7 @@
+"""
+For this assignment you will be writing a function called getMin() that has these properties:
+    parameters: userList (this is a list of integer values)
+    performs a while loop over the entire list that searches for the minimum values
+    once the minimum is found, returns that value
+"""
+
