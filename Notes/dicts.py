a0424d3dc3836f281997398c69cd6b40d14c3b24
venompit13 <mitch@venompit.com>
2026-03-12 12:20:57 -0400

    Create dicts.py

---
+++ Notes/dicts.py
@@ -0,0 +1,21 @@
+"""  
+dictionaries are hasmaps in other programmable languages
+
+info in dicts are stored using something called has function (really complicated math func)
+"""
+
+mydict = {}
+
+mydict2 = {"John":1000, "Joseph":0, "Mark":-250}
+
+print(mydict2["John"])
+
+print(mydict2["Mark"])
+
+mydict3 = {1:["a","b","c"], 2:{"foo":10}, 3:True}
+
+print(mydict3)
+print(mydict3[1][0])
+print(mydict3[2]["foo"])
+print(mydict3[3])
+
